ed79305fff1d434df9770cdfd84f1d304bcad0d2
UdaySimhadrii <tadakaudaysimhadri@gmail.com>
2026-07-18 14:12:34 +0530

    forloop

--- for.py
+++ for.py
@@ -1,3 +1,27 @@
 #print 10 numbers using for loop
+'''for i in range(10):
+    print(i)'''
+
+#sum of first 10 numbers using for loop
+'''sum = 0 
 for i in range(10):
-    print(i)
+    sum += i
+print("Sum of first 10 numbers is:", sum)'''
+
+#print even numbers between 1 to 20 using for loop
+'''for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)''' 
+#print odd numbers between 1 to 20 using for loop
+'''for i in range(1, 21):               
+    if i % 2 != 0:
+        print(i)''' 
+
+#calculate factorial of a number using for loop
+'''num = int(input("Enter a number: "))
+factorial = 1           
+for i in range(1, num + 1):
+    factorial *= i
+print("Factorial of", num, "is:", factorial)'''
+
+
