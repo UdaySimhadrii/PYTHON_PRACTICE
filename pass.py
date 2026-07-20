ea281bd1e708df67fddb59003f372298bb630fcc
UdaySimhadrii <tadakaudaysimhadri@gmail.com>
2026-07-20 15:56:34 +0530

    pass_keyword

---
+++ pass.py
@@ -0,0 +1,56 @@
+#list of integers in even
+'''n=10
+for i in range(2,11):
+    if i%2!=0:
+        pass
+    else:
+        print(i)'''
+
+#print prime number in list
+'''n=[3,6,8,9,46,7]
+for i in n:
+    for k in range(2,i):
+        if i%k==0:
+            pass
+            break
+    else:
+       print("the number is prime",i)'''
+
+
+'''class k:
+    pass'''
+
+#perfect square
+'''
+n=80
+if (n**0.5)**2==n:
+    print("perfect square")
+else:
+    pass'''
+
+#print ? statement
+
+'''sen = [
+    "How are you?",
+    "I am fine.",
+    "Where do you live?",
+    "Python is easy."
+]
+for i in sen:
+    if i.endswith("?"):
+        print(i)
+    else:
+        pass'''
+
+#print aall tesk completed
+n=[
+    ("python",True),
+    ("sql",False),
+    ("c",True)
+]
+
+for i,j in n:
+    if j:
+        print(i)
+    else:
+        pass
