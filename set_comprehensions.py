87ddcf8cf2640e1b56b2c99ddd553f00a018dc66
UdaySimhadrii <tadakaudaysimhadri@gmail.com>
2026-08-01 21:31:20 +0530

    completed

---
+++ set_comprehensions.py
@@ -0,0 +1,45 @@
+'''n={i for i in range(1,21) if i%2==0}
+print(n)'''
+
+'''str="I am uday how are you"
+wd=str.split()
+m={k for k in wd if len(k)>3}
+print(m)'''
+
+'''str="udaysimhadri"
+k={ch for ch in str if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'}
+print(k)'''
+
+'''m={i**2 for i in range(1,11) if (i**2)%3==0}
+print(m)'''
+'''
+a={1,2,3,5,7}
+b={2,3,567,9}
+c={i for i in a| b}
+print(c)'''
+
+'''str="UdaYsimHDri"
+k={ch for ch in str if 'A'<=ch<='Z'}
+print(k)'''
+
+'''str="I am uday how are  am you"
+wd=str.split()
+d=set()
+d={ch for ch in wd if ch not in d}
+print(d)'''
+'''
+k={i*3 for i in range(1,11)}
+print(k)'''
+
+
+'''k={"uday@gmail.com","k@gmail.com","llsnu"}
+n={i for i in k if i.endswith("@gmail.com")}
+print(n)'''
+
+
+'''n={-2,-4,-6,2,4,6}
+m={i for i in n if i>0 and i%2==0}
+print(m)'''
+
+
+
